340a4804a28bff1ec1ead1e36b97ff5e580e1d6f
Blathox <149165489+Blathox@users.noreply.github.com>
2025-01-13 11:22:45 +0100

    processdayaction

--- loup_garou/lib/game_logic/game_manager.dart
+++ loup_garou/lib/game_logic/game_manager.dart
@@ -1,7 +1,8 @@
 import 'package:loup_garou/game_logic/player.dart';
-
+import 'package:supabase_flutter/supabase_flutter.dart';
+import '../screens/chat_screen.dart';
 import 'phases.dart';
-
+import 'package:flutter/material.dart';
 class GameManager {
   List<Player> players = [];
   List<Player> alivePlayers = [];
@@ -9,24 +10,81 @@ class GameManager {
   List<Player> loversPlayers = [];
   GamePhase gamePhase = GamePhase();
 
+  final supabase = Supabase.instance.client;
+
   void startGame() {
     gamePhase.currentPhase = Phase.Day;
   }
 
+  
+  Future<List<Map<String, dynamic>>> fetchKilledPlayers() async {
+  try {
+ 
+    final response = await supabase
+        .from('PLAYERS')
+        .select('name, role')
+        .eq('killedAtNight', true);
+
+    if (response.isEmpty) {
+      return [];
+    }
+    return List<Map<String, dynamic>>.from(response);
+  } catch (error) {
+    print("Erreur lors de la récupération des joueurs tués : $error");
+    return [];
+  }
+}
+
+
+
 
   void processNightActions() {
-    // Gérer les actions des rôles la nuit
+    
   }
 
-  void processDayActions() {
-    // Gérer les votes ou discussions pendant le jour
+ 
+  void processDayActions() async {
+  print("Le village se réveille...");
+
+  List<Map<String, dynamic>> killedPlayers = await fetchKilledPlayers();
+
+  if (killedPlayers.isEmpty) {
+    print("Aucun joueur n'a été tué cette nuit.");
+  } else {
+    for (var player in killedPlayers) {
+      print("${player['name']} a été tué cette nuit. Son rôle était : ${player['role']}");
+      deadPlayers.add(Player(player['name'], player['role'], false));
+    }
   }
+
+void openChatScreen(BuildContext context, String gameId, String playerId) {
+  Navigator.push(
+    context,
+    MaterialPageRoute(
+      builder: (context) => ChatScreen(gameId: gameId, playerId: playerId),
+    ),
+  );
+}
+
+  await Future.delayed(const Duration(minutes: 1, seconds: 30));
+  print("Fin de la phase de discussion. Passage à la phase de vote.");
+
+  // for (Player player in alivePlayers) {
+  //   player.resetVote();
+  // }
+  // gamePhase.switchPhase();
+}
+
+
+
   void addPlayer(Player player) {
     players.add(player);
   }
+
   void removePlayer(Player player) {
     players.remove(player);
   }
+
   Player? getPlayerByName(String name) {
     for (Player player in players) {
       if (player.name == name) {
@@ -35,6 +93,7 @@ class GameManager {
     }
     return null;
   }
+
   void addPlayerLinked(Player player1, Player player2) {
     player1.isLinked = true;
     player2.isLinked = true;
